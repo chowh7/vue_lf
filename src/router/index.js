015e78f967418d091608678e0cb3b5aa00a5ff64
salted-hashbrown <105987464+salted-hashbrown@users.noreply.github.com>
2024-04-11 18:20:32 -0700

    added batch function

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import {createWebHistory, createRouter} from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 import HomeView from "../components/HomeView.vue";
 import SearchItem from "../components/SearchItem.vue";
 import ReportItem from "../components/ReportItem.vue";
 import ItemDetail from "../components/ItemDetail.vue";
-import ReportItemSuccess from "../components/ReportItemSuccess.vue"
+import ReportItemSuccess from "../components/ReportItemSuccess.vue";
+import BatchReport from "../components/BatchReport.vue"; // Import BatchReport component
 
 const routes = [
     {
@@ -11,12 +12,12 @@ const routes = [
         alias: "/home",
         name: "HomeView",
         component: HomeView
-    }, 
+    },
     {
         path: "/searchitem",
         name: "SearchItem",
         component: SearchItem
-    }, 
+    },
     {
         path: "/reportitem",
         name: "ReportItem",
@@ -31,12 +32,17 @@ const routes = [
         path: "/reportitemsuccess",
         name: "ReportItemSuccess",
         component: ReportItemSuccess
+    },
+    {
+        path: "/batchreport", // Add new route for BatchReport
+        name: "BatchReport",
+        component: BatchReport
     }
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
-})
+});
 
 export default router;
